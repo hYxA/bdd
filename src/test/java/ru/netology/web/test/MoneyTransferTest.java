591590ea5834fccedbb0bb8bc75267ce413c7901
hYxA <zybu@live.ru>
2022-09-06 22:46:20 +0300

    23-13

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web.test;
 
-import lombok.var;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
@@ -14,7 +13,7 @@ public class MoneyTransferTest {
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
-        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verifyInfo);
+//        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+//        verificationPage.validVerify(verifyInfo);
     }
 }
